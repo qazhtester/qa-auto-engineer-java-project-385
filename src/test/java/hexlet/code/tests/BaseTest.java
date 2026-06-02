587bdef15eba1c315fdc0b26dc5fb090ab3b7455
marchenko zhanna <zmarchenko@alfabank.ru>
2026-06-02 11:10:53 +0300

    delete headless option

--- src/test/java/hexlet/code/tests/BaseTest.java
+++ src/test/java/hexlet/code/tests/BaseTest.java
@@ -30,7 +30,6 @@ public abstract class BaseTest {
     @BeforeEach
     public void setupTest() {
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless");
         options.addArguments("--no-sandbox");
         driver = new ChromeDriver(options);
         driver.manage().window().maximize();
