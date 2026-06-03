527be40de3590e571b072c72c88c0465be446e8d
marchenko zhanna <zmarchenko@alfabank.ru>
2026-06-03 11:41:33 +0300

    add headless option 3

--- src/test/java/hexlet/code/tests/BaseTest.java
+++ src/test/java/hexlet/code/tests/BaseTest.java
@@ -30,8 +30,7 @@ public abstract class BaseTest {
     @BeforeEach
     public void setupTest() {
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless");
-        options.addArguments("--no-sandbox");
+        options.addArguments("--headless", "--no-sandbox");
         driver = new ChromeDriver(options);
         driver.manage().window().maximize();
     }
